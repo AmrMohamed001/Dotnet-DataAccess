3f7127724a1426bbbf8a67fe3b439458d45b00b9
Amr Mohamed <amrmohamd229@gmail.com>
2024-11-25 14:49:02 +0200

    reverse enginnering update

---
+++ ReverseEngineering/Models/TechTalkContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ReverseEngineering.Models;
+
+public partial class TechTalkContext : DbContext
+{
+    public TechTalkContext()
+    {
+    }
+
+    public TechTalkContext(DbContextOptions<TechTalkContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Speaker> Speakers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configurations = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        optionsBuilder.UseSqlServer(configurations.GetSection("ConnectionStrings").Value).LogTo(Console.WriteLine, LogLevel.Information);
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Events__3214EC077E6A1FA1");
+
+            entity.Property(e => e.EndAt).HasColumnType("datetime");
+            entity.Property(e => e.StartAt).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(255);
+
+            entity.HasOne(d => d.Speaker).WithMany(p => p.Events)
+                .HasForeignKey(d => d.SpeakerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Events__SpeakerI__1273C1CD");
+        });
+
+        modelBuilder.Entity<Speaker>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Speakers__3214EC07E443AE22");
+
+            entity.Property(e => e.FirstName).HasMaxLength(25);
+            entity.Property(e => e.LastName).HasMaxLength(25);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- ReverseEngineering/Program.cs
+++ ReverseEngineering/Program.cs
@@ -1,22 +1,40 @@
-﻿namespace ReverseEngineering;
+﻿using Microsoft.Data.SqlClient;
+using ReverseEngineering.Models;
+
+namespace ReverseEngineering;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Step #1: Package Manager Console (PMC)
-        //    Tools -> Nuget Package Manager -> Package Manager Console
+        // package manager console
+        //    Scaffold-DbContext '[Connection String]' [Provider] [1]-DataAnnotations [2]-Context AppDbContext [3]-ContextDir Data [4]-OutputDir Entities [5]-Schema dbo [6]-Tables Speakers
 
-        // Step #2: Package Manager Console (PMC) Tool 
-        //    Install-Package Microsoft.EntityFrameworkCore.Tools
+        // dotnet cli
+        // dotnet ef dbContext scaffold '[Connection String]' [Provider] --data-annotations --context AppDbContext --context-dir Data --output-dir Entities --schema dbo --table Speakers
 
-        // Step #3: Install Nuget Page on Project Microsoft.EntityFrameworkCore.Design
-        // Microsoft.EntityFrameworkCore.SqlServer
+        /*
+         * [1] to generate the context and the entities using data annotations not fluent api
+         * [2] to change the name of context
+         * [3] to specify the dir of the context
+         * [4] to specify the dir of entities
+         * [5] to generate the entities of specific schema
+         * [6] to generate only these tables
+         */
 
-        // Step #4: Install Provider in the project Microsoft.EntityFrameworkCore.SqlServer
+        var context = new TechTalkContext();
+        try
+        {
+            var speakers = context.Speakers.ToList();
+            foreach (var item in speakers)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
 
-        // Step #5: Run Command (Full)
-        //    Scaffold-DbContext '[Connection String]' [Provider]
-        Console.WriteLine("Hello, World!");
     }
 }
