f3c46cd881f46c079821324ea3703293ab7f7b55
Amr Mohamed <amrmohamd229@gmail.com>
2024-11-25 10:02:22 +0200

    Crud using efcore

---
+++ EfCoreOrm/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace efcoreOrm.Data;
+
+public class AppDbContext:DbContext
+{
+    public DbSet<Wallets> Wallets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        var configurations = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        optionsBuilder.UseSqlServer(configurations.GetSection("ConnectionStrings").Value);
+    }
+}
