3f2b4a3a2eb85de6f0816de20e4853ccbdd28151
Amr Mohamed <amrmohamd229@gmail.com>
2024-11-28 15:17:38 +0200

    Explicit loading related data

--- QueryData/Program.cs
+++ QueryData/Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using QueryData.Data;
+﻿using QueryData.Data;
 
 namespace QueryData
 {
@@ -12,41 +11,19 @@ namespace QueryData
                 // creating database without migrations
                 await context.Database.EnsureCreatedAsync();
 
-                #region one Join
-                //    var sectionId = 1;
-                //    var sectionQuery = context.Sections
-                //        .Include(x => x.Participants)
-                //        .Where(x => x.Id == sectionId);
-
-                //    Console.WriteLine(sectionQuery.ToQueryString());
-
-
-                //    var section = sectionQuery.FirstOrDefault();
-                //    Console.WriteLine($"section: {section.SectionName}");
-                //    Console.WriteLine($"--------------------");
-                //    foreach (var participant in section.Participants)
-                //        Console.WriteLine($"[{participant.Id}] {participant.FName} {participant.LName}");
-                #endregion
-
-                #region Many Joins
                 var sectionId = 1;
 
-                var sectionQuery = context.Sections
-                    .Include(x => x.Instructor)
-                    .ThenInclude(x => x.Office)
-                    .Where(x => x.Id == sectionId);
+                var section = context.Sections
+                    .FirstOrDefault(x => x.Id == sectionId);
 
-                Console.WriteLine(sectionQuery.ToQueryString());
+                // Explicit Loading
+                var query = context.Entry(section).Collection(x => x.Participants).Query();
 
+                Console.WriteLine($"section: {section.SectionName}");
+                Console.WriteLine($"--------------------");
 
-                var section = sectionQuery.FirstOrDefault();
-
-                Console.WriteLine($"section: {section.SectionName} " +
-                    $"[{section.Instructor.FName} " +
-                    $"{section.Instructor.LName} " +
-                    $"({section.Instructor.Office.OfficeName})]");
-
-                #endregion
+                foreach (var participant in query)
+                    Console.WriteLine($"[{participant.Id}] {participant.FName} {participant.LName}");
             }
         }
     }
