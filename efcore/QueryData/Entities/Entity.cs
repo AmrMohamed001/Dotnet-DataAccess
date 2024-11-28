68f6cb5fc3dc97eac2734feeb6b4639b648652ec
Amr Mohamed <amrmohamd229@gmail.com>
2024-11-28 11:42:20 +0200

    Revert "some organization"
    This reverts commit 2cf962097e929182aa522e1b475310c5d3bd1a73.

--- efcore/QueryData/Entities/Entity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EF015.QueryData.Entities
-{
-    public abstract class Entity
-    {
-        public int Id { get; set; }
-    }
-}
--- efcore/QueryData/Entities/Participant.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EF015.QueryData.Entities
-{
-    public class Participant : Entity
-    {
-        public string? FName { get; set; }
-        public string? LName { get; set; }
-        public ICollection<Section> Sections { get; set; } = new List<Section>();
-    }
-}
